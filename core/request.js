489651c887775b37af85397a10f3a27a4f169a39
Nianyi Wang <WangNianyi2001@outlook.com>
2021-10-04 21:44:53 +0800

    Enhanced Request class
    The original req object will be now included
    
    Empty components in the directory will be ignored

--- core/request.js
+++ core/request.js
@@ -3,8 +3,9 @@
 const URL = require('url');
 
 function Request(req) {
+	this.req = req;
 	this.url = URL.parse(req.url);
-	this.dir = this.url.pathname.slice(1).split('/').map(decodeURIComponent);
+	this.dir = this.url.pathname.slice(1).split('/').map(decodeURIComponent).filter(x => !!x);
 }
 
 module.exports = Request;
