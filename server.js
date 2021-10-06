16f556f331b6324880f69dc78d66a122fa3da348
Nianyi Wang <WangNianyi2001@outlook.com>
2021-10-06 13:42:08 +0800

    Remove port fallback

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const http = require('http');
 const getEntry = require('./core/getEntry');
 const readFile = require('./core/readFile');
-const getPort = () => (file => file ? +file.content : 8080)(readFile('./conf/port', 'utf-8'));
+const getPort = () => parseInt(readFile('./conf/port', 'utf-8').content);
 
 http.createServer((req, res) => {
 	const query = getEntry(req.url);
